a0161a3b934dc3fc800d4cd95dcdb3533d1fe584
Austinfer <ozzyandrew@hotmail.com>
2025-05-29 13:54:06 -0400

    comments

--- Assignment01/Question02/Main.c
+++ Assignment01/Question02/Main.c
@@ -6,14 +6,20 @@
 
 int main() {
     int n;
+
+    // Ask user how many products to generate
     printf("Enter number of products to create: ");
     scanf("%d", &n);
 
+    // Create a list of 'n' random products
     Node* productList = createList(n);
 
+    // Display all products in the list
     printf("\nProduct List:\n");
     traverseList(productList);
 
+    // Free all memory allocated for the list
     freeList(productList);
+
     return 0;
 }
--- Assignment01/Question02/product.c
+++ Assignment01/Question02/product.c
@@ -3,53 +3,67 @@
 #include <time.h>
 #include "product.h"
 
-// Function to create a list with n random products
+// Creates a list with 'n' random products
 Node* createList(int n) {
-    srand((unsigned int)time(NULL));
-    Node* head = NULL;
+    srand((unsigned int)time(NULL));  // Seed random number generator
+
+    Node* head = NULL;  // Start with an empty list
+
+    // Loop to create 'n' products
     for (int i = 0; i < n; i++) {
         Product p;
-        p.id = rand() % 100 + 1;     // ID between 1–100
-        p.qty = rand() % 51;         // Qty between 0–50
+        p.id = rand() % 100 + 1;    // Random ID from 1 to 100
+        p.qty = rand() % 51;        // Random quantity from 0 to 50
+
+        // Insert each product at the end of the list
         head = insertAtEnd(head, p);
     }
-    return head;
+
+    return head;  // Return the head of the list
 }
 
-// Insert a product at the end of the list
+// Inserts a product at the end of the linked list
 Node* insertAtEnd(Node* head, Product newProd) {
+    // Allocate memory for new node
     Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = newProd;
-    newNode->next = NULL;
+    newNode->data = newProd;  // Assign product data
+    newNode->next = NULL;     // This node will be at the end, so next is NULL
 
+    // If list is empty, return new node as head
     if (head == NULL) {
         return newNode;
     }
 
+    // Otherwise, traverse to the end of the list
     Node* temp = head;
     while (temp->next != NULL) {
         temp = temp->next;
     }
 
+    // Link the last node to the new node
     temp->next = newNode;
-    return head;
+    return head;  // Return the unchanged head
 }
 
-// Traverse and print the list
+// Traverses the list and prints product data
 void traverseList(Node* head) {
     Node* temp = head;
+
+    // Loop until the end of the list
     while (temp != NULL) {
         printf("Product ID: %d, Quantity: %d\n", temp->data.id, temp->data.qty);
-        temp = temp->next;
+        temp = temp->next;  // Move to the next node
     }
 }
 
-// Free all allocated memory
+// Frees memory used by the entire linked list
 void freeList(Node* head) {
     Node* temp;
+
+    // Loop through and free each node
     while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
+        temp = head;         // Store current node
+        head = head->next;   // Move to next node
+        free(temp);          // Free memory
     }
 }
